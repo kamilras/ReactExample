4a68ac3d688430cdc61f3e9ecc7d0c07772488f3
Kamil Ras <kamilras@gmail.com>
2022-09-25 23:27:44 +0200

    ukończenie aplikacji

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
+import React, { Component } from "react";
+import { Header } from "./Header";
+import { ListItems } from "./ListItems";
 
+export default class App extends Component {
+  state = {
+    items: [
+      { id: 1, name: "herbata", active: true },
+      { id: 2, name: "ziemniaki", active: false },
+      { id: 3, name: "cukier", active: false },
+      { id: 4, name: "mąka", active: false },
+      { id: 5, name: "woda", active: false },
+      { id: 6, name: "ciasto", active: false },
+      { id: 7, name: "kawa", active: false },
+    ]
+  }
+
+  handleChangeStatus = (id) => {
+    const items = this.state.items.map(item => {
+      if (id === item.id) {
+        item.active = !item.active
+      }
+      return item
+    })
+
+    this.setState({
+      items: items
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Header items={this.state.items}></Header>
+        <ListItems items={this.state.items} changeStatus={this.handleChangeStatus}></ListItems>
+      </>
+    )
+  }
+}
---
+++ src/Item.js
@@ -0,0 +1,9 @@
+export const Item = (props) => {
+    return (
+        <li className={props.active ? "enabled" : "disabled"}
+            onClick={() => props.changeStatus(props.id)}
+        >
+            {props.name}
+        </li>
+    )
+}
