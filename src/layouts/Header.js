e6ba39cdcfa256e46c98e474ae3de41fe52dea2a
Kamil Ras <kamilras@gmail.com>
2022-09-26 19:21:03 +0200

    Add picture to header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Img from '../components/Img'
+
+import '../styles/Header.css'
+
+import img1 from '../images/header1.jpg';
+import img2 from '../images/header2.jpg';
+import img3 from '../images/header3.jpg';
+
+
 const Header = (props) => {
     return (
-        <div> Header</div>);
+        <>
+            <Routes>
+                <Route path="/" element={<Img img={img1} alt={"ulica w mieście"} />} />
+                <Route path="/products" element={<Img img={img2} alt={"ulica w mieście"} />} />
+                <Route path="/contact" element={<Img img={img3} alt={"ulica w mieście"} />} />
+                <Route path="/admin" element={<Img img={img3} alt={"ulica w mieście"} />} />
+                <Route path="/admin" element={<Img img={img3} alt={"ulica w mieście"} />} />
+                {/* <Route path="/" render={() => (<img src={img1} alt={"ulica w mieście"} />)} /> */}
+            </Routes>
+        </>
+    );
 }
 
 export default Header;
