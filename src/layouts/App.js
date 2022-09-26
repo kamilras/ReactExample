9c2bbcb77fa4085f52c06726d3f57edda59e0fd8
Kamil Ras <kamilras@gmail.com>
2022-09-26 18:13:35 +0200

    utworzenie menu

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <div>
+                <div className="app">
                     <header>
                         {<Header />}
                     </header>
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import "../styles/Navigation.css"
+
+const list = [
+    { name: "start", path: "/", end: true },
+    { name: "produkty", path: "/products" },
+    { name: "kontakt", path: "/contact" },
+    { name: "panel admina", path: "/admin" },
+
+]
+
 const Navigation = (props) => {
+
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path} end={item.end ? true : false}>{item.name}</NavLink>
+        </li>
+    ))
     return (
-        <div>Navigation</div>
+        <nav className='main'>
+            <ul>
+                {menu}
+            </ul>
+        </nav>
     );
 }
 
